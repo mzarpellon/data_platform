36485fc792708eff3e235de70d94cb37be1b6974
mzarpellon <michael.zapelao@gmail.com>
2026-01-28 17:39:45 -0300

    corrigido nome de variavel aws

--- airflow/dags/ingest_postgres_to_minio.py
+++ airflow/dags/ingest_postgres_to_minio.py
@@ -67,7 +67,7 @@ def extract_and_load():
         "s3",
         endpoint_url=os.environ["S3_ENDPOINT"],
         aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-        aws_secret_acess_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
     )
 
     ingestion_date = datetime.utcnow().date().isoformat()
